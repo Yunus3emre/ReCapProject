ff4e248f32ed72b8b2a56bc9af6544e5d942fc9b
Yunus Emre Ayan <67464029+Yunus3emre@users.noreply.github.com>
2021-03-26 16:43:54 +0300

    gun18

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -24,7 +25,8 @@ namespace WebAPI.Controllers
 
         [HttpGet("getall")]
         public IActionResult GetAll()
-        {            
+        {
+            Thread.Sleep(500);
             var result = _carService.GetAll();
             if (result.Success)
             {
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -38,15 +38,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            //services.AddSingleton<ICarService,CarManager>();
-            //services.AddSingleton<IBrandService,BrandManager>();
-
-            //services.AddSingleton<ICarDal,EfCarDal>();
-            //services.AddSingleton<IBrandDal,EfBrandDal>();
-
-            
-
-
+            services.AddCors();
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -76,6 +68,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
